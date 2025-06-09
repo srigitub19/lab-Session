c35b93815009eef61b63a4cb29b4a9a14a0ae435
srigitub19 <srinathmat07@gmail.com>
2025-06-09 15:01:24 +0530

    Add files via upload
    new

---
+++ row.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class encode{
+    public static String enc(String msg,int x){
+        StringBuilder result=new StringBuilder();
+        int l=msg.length();
+        int digit=l/x;
+        Scanner scan= new Scanner(System.in);
+        int y=scan.nextInt();
+        for(int i=0;i<length;i++){
+            for
+        }
+    }
+}
